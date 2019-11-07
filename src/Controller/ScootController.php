1d7ed68bd1889a1d3bbfd336ba183f65fd2a1448
Noled <e.morlon@codeur.online>
2019-11-07 11:25:42 +0100

    add commentaries

--- src/Controller/ScootController.php
+++ src/Controller/ScootController.php
@@ -14,10 +14,10 @@ class ScootController extends AbstractController
      */
     public function home()
     {
-        return $this->render('scoot/home.html.twig', [
-          'title' => 'Accueil',
-          'arrow' => ''
-        ]);
+      return $this->render('scoot/home.html.twig', [
+        'title' => 'Accueil',
+        'arrow' => '' // Hide the back arrow if on main page
+      ]);
     }
 
     /**
@@ -28,11 +28,9 @@ class ScootController extends AbstractController
       $creerObjet = new Objet();
       $form = $this -> createForm(CreerObjetType::class, $creerObjet);
 
-
         return $this->render('scoot/inventaire.html.twig', [
-            'form' => $form->createView(),
-            'title' => 'Inventaire',
-
+          'form' => $form->createView(),
+          'title' => 'Inventaire'
         ]);
     }
 
@@ -42,7 +40,7 @@ class ScootController extends AbstractController
     public function reservez()
     {
       return $this->render('scoot/reservez.html.twig', [
-        'title' => 'Je réserve',
+        'title' => 'Je réserve'
       ]);
     }
 
