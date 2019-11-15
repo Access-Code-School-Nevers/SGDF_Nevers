5d1a51a0b9801e96456f0fc1f3e26250da51225e
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-11-15 11:23:11 +0100

    delate object input in article périssable/form>

--- src/Form/ArticlePerissable.php
+++ src/Form/ArticlePerissable.php
@@ -4,9 +4,6 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,18 +14,9 @@ class ArticlePerissable extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('objet', TextType::class, [
-          'attr' => array('list' => 'objects')
-        ])
         ->add('peremption', DateType::class, array(
           'widget' => 'single_text',
         ))
-<<<<<<< HEAD
-=======
-        ->add('scan', ButtonType::class, [
-          'attr' => array('class' => 'button_scan_restituer')
-        ])
->>>>>>> 01d1db2ffc10c78d0458a7a9c4c1366ed27ea55c
         ->add('ajouter', SubmitType::class, [
           'attr' => ['class' => 'w-100 btn-primary main-blue-color second-blue-color']
         ])
